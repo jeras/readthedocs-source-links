815e29f03371569f519e9ffc949ec0b69d9e855a
Iztok Jeras <iztok.jeras@gmail.com>
2017-06-20 12:20:10 +0200

    moved code role code into extension

--- conf.py
+++ conf.py
@@ -19,6 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.') + '/_extensions')
 
 # -- General configuration ------------------------------------------------
 
@@ -33,6 +34,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'sphinx.ext.todo',
+    'github',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -108,35 +110,6 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-
-# -- Custom extension for GitHub links ------------------------------------
-
-# based on http://protips.readthedocs.io/link-roles.html
-
-from docutils import nodes
-#from repo_util import run_cmd_get_output
-
-#def get_github_rev():
-#    path = run_cmd_get_output('git rev-parse --short HEAD')
-#    tag  = run_cmd_get_output('git describe --exact-match')
-#    print 'Git commit ID: ', path
-#    if len(tag):
-#        print 'Git tag: ', tag
-#        path = tag
-#    return path
-
-def setup(app):
-    baseurl = 'https://github.com/jeras/readthedocs-source-links'
-    #rev = get_github_rev()
-    rev = 'master'
-    app.add_role('source', autolink('{}/blob/{}/%s'.format(baseurl, rev)))
-
-def autolink(pattern):
-    def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-        url = pattern % (text,)
-        node = nodes.reference(rawtext, text, refuri=url, **options)
-        return [node], []
-    return role
 
 # -- Options for HTML output ----------------------------------------------
 
